46e6d5332b3c2dbdf1b21857f160846ce6136b95
alen <alen.dunn1@gmail.com>
2022-03-27 21:09:20 -0400

    change session secret

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const hbs = exphbs.create({});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: 'super secret secret',
+    secret: process.env.SESSION_SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
